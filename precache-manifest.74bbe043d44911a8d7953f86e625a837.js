602730e94ecd1e7802e45bff6829e7d76319e3f6
nduhiu17 <nduhiu254@gmail.com>
2020-07-19 07:46:36 -0700

    Updates

---
+++ precache-manifest.74bbe043d44911a8d7953f86e625a837.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0820f80c1664bca86f5f69350bf31bc9",
+    "url": "/materialize-css-form/index.html"
+  },
+  {
+    "revision": "db35e91d382c5f4fd32b",
+    "url": "/materialize-css-form/static/css/main.a3540e59.chunk.css"
+  },
+  {
+    "revision": "16c5249150f6dd4e1dff",
+    "url": "/materialize-css-form/static/js/2.d97be5f9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/materialize-css-form/static/js/2.d97be5f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db35e91d382c5f4fd32b",
+    "url": "/materialize-css-form/static/js/main.b6ce760e.chunk.js"
+  },
+  {
+    "revision": "8e40f069c663bda88ac1",
+    "url": "/materialize-css-form/static/js/runtime-main.a110b938.js"
+  },
+  {
+    "revision": "2b7f244ae0184170cbe91dbf14081eb5",
+    "url": "/materialize-css-form/static/media/image.2b7f244a.png"
+  }
+]);
